fc571dd4b554388d3026d96b1cf36287db665574
Tiger Wang <tigerwang@outlook.com>
2021-11-09 04:00:24 +0000

    WIP

--- src/main/java/com/newpathfly/flightsearchwebapp/view/MainView.java
+++ src/main/java/com/newpathfly/flightsearchwebapp/view/MainView.java
@@ -1,5 +1,20 @@
 package com.newpathfly.flightsearchwebapp.view;
 
-public class MainView{
-    
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "")
+public class MainView extends VerticalLayout {
+
+    private final H1 _headline;
+
+    public MainView() {
+
+        _headline = new H1("Welcome to the Flight Search Web App");
+
+        add( //
+                _headline //
+        );
+    }
 }
