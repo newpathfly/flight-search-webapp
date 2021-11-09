ae47113d587918aad7c732f09ca058fc22fb39ed
Tiger Wang <tigerwang@outlook.com>
2021-11-09 03:11:13 +0000

    wip

---
+++ src/test/java/com/newpathfly/flightsearchwebapp/FlightSearchWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newpathfly.flightsearchwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
